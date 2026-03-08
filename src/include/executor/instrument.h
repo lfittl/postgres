79c4489b9107e3900d0dd94978a622f602e1134e
Lukas Fittl <lukas@fittl.com>
2026-03-07 17:52:24 -0800

    instrumentation: Optimize ExecProcNodeInstr instructions by inlining
    For most queries, the bulk of the overhead of EXPLAIN ANALYZE happens in
    ExecProcNodeInstr when starting/stopping instrumentation for that node.
    
    Previously each ExecProcNodeInstr would check which instrumentation
    options are active in the InstrStartNode/InstrStopNode calls, and do the
    corresponding work (timers, instrumentation stack, etc.). These
    conditionals being checked for each tuple being emitted add up, and cause
    non-optimal set of instructions to be generated by the compiler.
    
    Because we already have an existing mechanism to specify a function
    pointer when instrumentation is enabled, we can instead create specialized
    functions that are tailored to the instrumentation options enabled, and
    avoid conditionals on subsequent ExecProcNodeInstr calls. This results in
    the overhead for EXPLAIN (ANALYZE, TIMING OFF, BUFFERS OFF) for a stress
    test with a large COUNT(*) that does many ExecProcNode calls from ~ 20% on
    top of actual runtime to ~ 3%. When using BUFFERS ON the same query goes
    from ~ 20% to ~ 10% on top of actual runtime.
    
    Author: Lukas Fittl <lukas@fittl.com>
    Reviewed-by: Zsolt Parragi <zsolt.parragi@percona.com>
    Discussion: https://www.postgresql.org/message-id/flat/CAP53PkxFP7i7-wy98ZmEJ11edYq-RrPvJoa4kzGhBBjERA4Nyw%40mail.gmail.com#e8dfd018a07d7f8d41565a079d40c564

--- src/include/executor/instrument.h
+++ src/include/executor/instrument.h
@@ -287,6 +287,11 @@ extern void InstrUpdateTupleCount(NodeInstrumentation *instr, double nTuples);
 extern void InstrEndLoop(NodeInstrumentation *instr);
 extern void InstrAggNode(NodeInstrumentation *dst, NodeInstrumentation *add);
 
+typedef struct TupleTableSlot TupleTableSlot;
+typedef struct PlanState PlanState;
+typedef TupleTableSlot *(*ExecProcNodeMtd) (PlanState *pstate);
+extern ExecProcNodeMtd InstrNodeSetupExecProcNode(NodeInstrumentation *instr);
+
 extern TriggerInstrumentation *InstrAllocTrigger(int n, int instrument_options);
 extern void InstrStartTrigger(QueryInstrumentation *qinstr,
 							  TriggerInstrumentation *tginstr);
