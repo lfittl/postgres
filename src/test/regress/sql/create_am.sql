4d43f8e2d5d22c4e72784065e8a356d17eb85dd2
Andrei V. Lepikhov <lepihov@gmail.com>
2026-04-02 19:26:34 +0200

    Adjust query plans changed

--- src/test/regress/sql/create_am.sql
+++ src/test/regress/sql/create_am.sql
@@ -146,9 +146,9 @@ SELECT
     pc.relkind,
     pa.amname,
     CASE WHEN relkind = 't' THEN
-        (SELECT 'toast for ' || relname::regclass FROM pg_class pcm WHERE pcm.reltoastrelid = pc.oid)
+        (SELECT 'toast for ' || pcm.oid::regclass FROM pg_class pcm WHERE pcm.reltoastrelid = pc.oid)
     ELSE
-        relname::regclass::text
+        pc.oid::regclass::text
     END COLLATE "C" AS relname
 FROM pg_class AS pc,
     pg_am AS pa
